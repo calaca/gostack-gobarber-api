1c6514b22b43cb53eb732907b56ff24cb2eed6bd
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-10-15 19:49:40 -0300

    Models loader

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World!!!' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Lorena Calaça',
+    email: 'lorena.calaca.od@gmail.com',
+    password_hash: 'senha123',
+  });
+
+  res.json(user);
+});
 
 export default routes;
