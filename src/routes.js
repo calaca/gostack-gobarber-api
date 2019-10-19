d548b1044a90010cf6b5e6deff728ba62e6ba801
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-10-19 16:12:18 -0300

    List all providers

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -13,7 +14,10 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
