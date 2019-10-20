e023de8498c146891b9582c33f8cd48790b12442
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-10-19 23:59:30 -0300

    Configure nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,11 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'a9b13460ab71bb',
+    pass: 'a7603dfaf81475',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
