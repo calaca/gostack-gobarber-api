275d8a377eccdfb37349df5df4edce10e013f442
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-10-19 21:26:57 -0300

    Configure mongodb

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,19 @@
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: '12345',
-  database: 'gobarber',
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
+  postgres: {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '12345',
+    database: 'gobarber',
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  },
+  mongo: {
+    username: 'mongo',
+    password: '12345',
+    database: 'gobarber',
   },
 };
--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
@@ -9,14 +10,23 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
-    this.connection = new Sequelize(databaseConfig);
+    this.connection = new Sequelize(databaseConfig.postgres);
 
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    const { username, password, database } = databaseConfig.mongo;
+    this.mongoConnection = mongoose.connect(
+      `mongodb://${username}:${password}@localhost:27017/${database}`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
   }
 }
 
